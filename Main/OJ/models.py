104967d5d7c6113f4372089773cd85dfa6b32b62
SachitSankhe <sachitsankhe@gmail.com>
2022-06-29 23:24:39 +0530

    Day1-Created models and migrated them and used them in shell also created views and displayed list of problems according to different orders.

--- Main/OJ/models.py
+++ Main/OJ/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
+
+
+class User(models.Model):
+    fname = models.CharField('First Name', max_length=30)
+    sname = models.CharField('First Name', max_length=30)
+
+    # User_totalscore = models.FloatField()
+    def __str__(self):
+        return self.fname
+
+
+class Problems(models.Model):
+    problem_id = models.BigAutoField(
+        primary_key=True, db_column="Problem ID")
+    problem_name = models.CharField(max_length=50, db_column="Problem Name")
+    problem_statement = models.CharField(
+        max_length=400, db_column="Problem Statement")
+    problem_code = models.CharField(max_length=200, db_column="Code")
+    problem_status = models.BooleanField(
+        db_column="Solve_status", default=False)
+    problem_level = models.CharField(max_length=10, db_column="Problem level")
+    # problem_maxscore = models.FloatField(db_column="Max Score")
+    # problem_scored = models.FloatField(db_column="Scored")
+
+    def __str__(self):
+        return self.problem_name
+
+
+class TestCases(models.Model):
+    problem_id = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    input = models.FileField('Input', max_length=200)
+    output = models.CharField('Output', max_length=200)
+
+    def __str__(self):
+        return self.input
+
+
+class Solutions(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    problem_id = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField('Submitted on')
+    Verdict = models.CharField('Verdict', max_length=20)
+
+    def __str__(self):
+        return self.Verdict
--- Main/OJ/views.py
+++ Main/OJ/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from .models import Problems
 #Create your views here.
+
+def index(request):
+    context = {
+        'Problem_List' : Problems.objects.order_by('-problem_level')[:5]
+    }
+    return render(request,'OJ\index.html',context)
