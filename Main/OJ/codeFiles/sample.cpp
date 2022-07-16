6ab2b43d88bcfe1e932dd66b9e764f956393471b
SachitSankhe <sachitsankhe@gmail.com>
2022-07-16 16:32:37 +0530

    The result processing has improved

--- Main/OJ/codeFiles/sample.cpp
+++ Main/OJ/codeFiles/sample.cpp
@@ -1,13 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-        /* code */
-        while (true)
-        {
-                /* code */
-        couot <<"Hello" << endl;
+int main() {
+  int t;
+  cin >> t;
+  while (t>0)
+  {
+    int first_number, second_number, sum;
+    
+  cin >> first_number >> second_number;
 
-        }
-        
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  sum = first_number + second_number;
+
+  // prints sum 
+  cout << sum <<endl;
+  t--;
+  }   
+  return 0;
 }
