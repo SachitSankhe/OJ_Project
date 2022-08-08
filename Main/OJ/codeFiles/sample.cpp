ae253479399d76f886631a38157ba0403b1675bf
Sachit_Sankhe <sachitsankhe@gmail.com>
2022-08-08 15:26:54 +0530

    Some more changes

--- Main/OJ/codeFiles/sample.cpp
+++ Main/OJ/codeFiles/sample.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int t;
-  cin >> t;
-  while (t>0)
-  {
-    int first_number, second_number, sum;
-    
-  cin >> first_number >> second_number;
-
-  // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number;
-
-  // prints sum 
-  cout << sum <<endl;
-  t--;
-  }   
-  return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (true)
+    {
+        cout << "TLE" << endl;
+    }
+    return 0;
 }
